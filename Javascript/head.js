1176676c23cbca6bbd7786ca58a3e1c7137ccec8
Miguel Coelho <mignownow@gmail.com>
2019-02-25 01:37:22 +0000

    Image Slider
    Arrows added to images slider. Work in progress for image slider on javascript.

--- Javascript/head.js
+++ Javascript/head.js
@@ -3,7 +3,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(400, 400) // sets the renderer size to window size
 renderer.setPixelRatio(window.devicePixelRatio) // sets the pixel whatever the device is
-renderer.setClearColor(0x070000) // renderer color
+renderer.setClearColor(0xE84526) // renderer color
 
 // ADD RENDERER TO ELEMENT
 const sectionTag = document.querySelector(".head-container")
@@ -19,10 +19,10 @@ camera.position.z = -70 // changes the position of the camera in the Z axis
 camera.position.x = 20
 
 // ADD LIGHTS
-const ambientLight = new THREE.AmbientLight(0x00DDFF)
+const ambientLight = new THREE.AmbientLight(0x38AAF7)
 scene.add(ambientLight)
 
-const directionalLight = new THREE.DirectionalLight(0xff521b, 0.6) // light like sun direction
+const directionalLight = new THREE.DirectionalLight(0xb19748, 0.6) // light like sun direction
 directionalLight.position.set(50, 100, -100) // by default the direction is from the top, in here we are changing the angle
 scene.add(directionalLight)
 
@@ -30,18 +30,18 @@ scene.add(directionalLight)
 // ADD OBJECT LOADER AND MTL LOADER
 const objLoader = new THREE.OBJLoader()
 
-let earth = null // this varible is null, because we haven't load the object yet
+let head = null // this varible is null, because we haven't load the object yet
 
 // In here we are adding a Load method to the file where we can pass more info. It's like adding an event listener ---> Load eath.mtl and then do this
   objLoader.load("data/male_head.obj", function(obj) { // Loads the obj and adds it to the scene
-    earth = obj
+    head = obj
     scene.add(obj)
   })
 
 // ADD ANIMATION
 const animate = function () {
-  if (earth) { // if earth is not null rotate
-    earth.rotateY(0.01)
+  if (head) { // if head is not null rotate
+    head.rotateY(0.01)
   }
   camera.lookAt(scene.position) // tells the camera to focus on scene position
   renderer.render(scene, camera) // What we want to do in this loop? we want to render the scene and the camera
--- Javascript/interaction.js
+++ Javascript/interaction.js
@@ -1,8 +1,14 @@
 const workLink = document.querySelector('.item-1').addEventListener('click', closeScene);
 const canvasScene = document.querySelector('canvas');
+const headCont = document.querySelector('.head-container');
+const sliderWork = document.querySelector('.work-slider');
 
 function closeScene() {
-   canvasScene.style.opacity = "0"
    canvasScene.style.transition = "all .4s ease-out .4s";
-   canvasScene.style.display = "block";
+   canvasScene.style.opacity = "0";
+   canvasScene.style.visibility = "hidden";
+
+   sliderWork.style.transform = "translateY(-540px)";
+   sliderWork.style.opacity = "1";
+   sliderWork.style.transition = "all .6s ease-in .5s";
  }
