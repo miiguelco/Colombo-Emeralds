c506f59b7d991901643ba6b7b6a5670a074daf07
Miguel Coelho <mignownow@gmail.com>
2019-02-26 01:16:47 +0000

    Play Button
    Play and pause user interaction complete.

--- Javascript/interaction.js
+++ Javascript/interaction.js
@@ -1,3 +1,5 @@
+
+
 const workLink = document.querySelector('.item-1').addEventListener('click', closeScene);
 const canvasScene = document.querySelector('canvas');
 const headCont = document.querySelector('.head-container');
@@ -12,3 +14,38 @@ function closeScene() {
    sliderWork.style.opacity = "1";
    sliderWork.style.transition = "all .6s ease-in .5s";
  }
+
+
+ // PLAY AND PAUSE BUTTON
+
+let playButton = document.querySelector('.fa-play').addEventListener('click', playSong);
+let pauseButton = document.querySelector('.fa-pause').addEventListener('click', playSong);
+let player = document.getElementById('play');
+let iconPlay = document.querySelector('.fa-play');
+let iconPause = document.querySelector('.fa-pause');
+let playToPlause = iconPlay.addEventListener('click', change);
+let pauseToPlay = iconPause.addEventListener('click', change);
+let artist = document.querySelector('.song-playing');
+
+
+
+function playSong() { // plays and pauses the song
+    if(player.paused) {
+       player.play();
+  } else {
+    player.pause();
+  }
+}
+
+function change() { // changes icon from play to pause
+   if(iconPlay.style.visibility === "hidden") {
+      iconPlay.style.visibility = "visible";
+      iconPause.style.visibility = "hidden";
+      artist.style.visibility = "hidden";
+  } else {
+     iconPlay.style.visibility = "hidden";
+     iconPause.style.visibility = "visible";
+     artist.style.visibility = "visible";
+
+  }
+}
